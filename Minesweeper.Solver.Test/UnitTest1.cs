c3b32f7e22720ed340f546079035baa7eed80394
OthmanAlkhamra <Jira@599>
2016-11-30 11:52:52 +0200

    Add project files.

---
+++ Minesweeper.Solver.Test/UnitTest1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Minesweeper.Solver;
+
+namespace Minesweeper.Solver.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestProperty("TopCornerSquareTesting","-1")]
+        public void GetLocationValue_TopCornerSquareWithMine_Success()
+        {
+            int[,] inputBoard = new int [,]{ { -1, 0, 0 }, { 0, -1, 0 }, { 0, 0, 0 } };
+            MinesweeperSolver minesweeperSolver = new MinesweeperSolver(3, 3, inputBoard);
+
+            int locationValue = minesweeperSolver.GetLocationValue(0, 0);
+            Assert.AreEqual(-1, locationValue);
+        }
+
+        [TestMethod]
+        [TestProperty("Non-Existing location", "")]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void SolveGame_With_Success()
+        {
+            int[,] inputBoard = new int[,] { { -1, -1, -1, 0, -1 } };
+            MinesweeperSolver minesweeperSolver = new MinesweeperSolver(3, 3, inputBoard);
+
+            int locationValue = minesweeperSolver.GetLocationValue(10, 10);
+            
+        }
+
+        [TestMethod]
+        [TestProperty("BottomCornerSquareTesting", "1")]
+        public void GetLocationValue_BottomCornerSquareWithMine_Success()
+        {
+            int[,] inputBoard = new int[,] { { -1, 0, 0 }, { 0, -1, 0 }, { 0, 0, 0 } };
+            MinesweeperSolver minesweeperSolver = new MinesweeperSolver(3, 3, inputBoard);
+
+            int locationValue = minesweeperSolver.GetLocationValue(2, 2);
+            Assert.AreEqual(1, locationValue);
+        }
+
+        [TestMethod]
+        [TestProperty("TopCornerSquareTesting", "1")]
+        public void GetLocationValue_CornerSquareWithoutMine_Success()
+        {
+            int[,] inputBoard = new int[,] { { 0, 0, 0 }, { 0, -1, 0 }, { 0, 0, 0 } };
+            MinesweeperSolver minesweeperSolver = new MinesweeperSolver(3, 3, inputBoard);
+
+            int locationValue = minesweeperSolver.GetLocationValue(0, 0);
+            Assert.AreEqual(1, locationValue);
+        }
+
+        [TestMethod]
+        [TestProperty("CornerSquareTesting", "5")]
+        public void GetLocationValue_SquareAllSurroundedByMines_Success()
+        {
+            int[,] inputBoard = new int[,] { { -1, 0, -1 }, { -1, -1, -1 }, { 0, 0, 0 } };
+            MinesweeperSolver minesweeperSolver = new MinesweeperSolver(3, 3, inputBoard);
+
+            int locationValue = minesweeperSolver.GetLocationValue(0, 1);
+            Assert.AreEqual(5, locationValue);
+        }
+
+        [TestMethod]
+        [TestProperty("MiddleSquareTestingFullySurroundedWithMines", "0")]
+        public void GetLocationValue_SquareInMiddleAllSurroundedByMines_Success()
+        {
+            int[,] inputBoard = new int[,] { { -1, -1, -1 }, { -1, 0, -1 }, { -1, -1, -1 },  { 0,0,0} };
+            MinesweeperSolver minesweeperSolver = new MinesweeperSolver(3, 3, inputBoard);
+
+            int locationValue = minesweeperSolver.GetLocationValue(1, 1);
+            Assert.AreEqual(8, locationValue);
+        }
+
+        [TestMethod]
+        [TestProperty("MiddleSquareTestingWithoutMines", "0")]
+        public void GetLocationValue_SquareInMiddleWithoutMines_Success()
+        {
+            int[,] inputBoard = new int[,] { {0,0,0 }, { 0, 0, 0 }, { 0,0,0}, { -1, 0, -1 } };
+            MinesweeperSolver minesweeperSolver = new MinesweeperSolver(3, 3, inputBoard);
+
+            int locationValue = minesweeperSolver.GetLocationValue(1, 1);
+            Assert.AreEqual(0, locationValue);
+        }
+
+
+        [TestMethod]
+        [TestProperty("SolveBoardWithEmptyMines", "All Zeors")]
+        public void SolveGame_WithEmptyMines_Success()
+        {
+            int[,] inputBoard = new int[,] { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 }};
+            int[,] expectedOutputBoard = new int[,] { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
+            MinesweeperSolver minesweeperSolver = new MinesweeperSolver(3, 3, inputBoard);
+
+            int[,] outputBoard = minesweeperSolver.SolveGame();
+
+            CollectionAssert.AreEqual(expectedOutputBoard, outputBoard);
+        }
+
+
+        [TestMethod]
+        [TestProperty("WithAllMines", "All -1")]
+        public void SolveGame_WithAllMines_Success()
+        {
+            int[,] inputBoard = new int[,] { { -1, -1, -1 }, { -1, -1, -1 }, { -1, -1, -1 } };
+            int[,] expectedOutputBoard = new int[,] { { -1, -1, -1 }, { -1, -1, -1 }, { -1, -1, -1 } };
+            MinesweeperSolver minesweeperSolver = new MinesweeperSolver(3, 3, inputBoard);
+
+            int[,] outputBoard = minesweeperSolver.SolveGame();
+
+            CollectionAssert.AreEqual(expectedOutputBoard, outputBoard);
+        }
+
+
+        [TestMethod]
+        [TestProperty("WithOneSquare", "Same square value")]
+        public void SolveGame_WithOneSquare_Success()
+        {
+            int[,] inputBoard = new int[,] { { -1 } };
+            int[,] expectedOutputBoard = new int[,] { { -1 } };
+            MinesweeperSolver minesweeperSolver = new MinesweeperSolver(1, 1, inputBoard);
+
+            int[,] outputBoard = minesweeperSolver.SolveGame();
+
+            CollectionAssert.AreEqual(expectedOutputBoard, outputBoard);
+        }
+
+        [TestMethod]
+        [TestProperty("WithOneColumn", "")]
+        public void SolveGame_WithOneColumn_Success()
+        {
+            int[,] inputBoard = new int[,] { { -1 }, { -1 }, { -1 }, { 0 }, { -1 } };
+            int[,] expectedOutputBoard = new int[,] { { -1 }, { -1 }, { -1 }, { 2 }, { -1 } };
+            MinesweeperSolver minesweeperSolver = new MinesweeperSolver(1, 5, inputBoard);
+
+            int[,] outputBoard = minesweeperSolver.SolveGame();
+
+            CollectionAssert.AreEqual(expectedOutputBoard, outputBoard);
+        }
+
+        [TestMethod]
+        [TestProperty("WithOneRow", "")]
+        public void SolveGame_WithOneRow_Success()
+        {
+            int[,] inputBoard = new int[,] { { -1 , -1 , -1 , 0 ,-1 } };
+            int[,] expectedOutputBoard = new int[,] { { -1, -1, -1, 2, -1 } };
+            MinesweeperSolver minesweeperSolver = new MinesweeperSolver(5, 1, inputBoard);
+
+            int[,] outputBoard = minesweeperSolver.SolveGame();
+      
+            CollectionAssert.AreEqual(expectedOutputBoard, outputBoard);
+        }
+
+
+
+    }
+}
---
+++ Minesweeper.Solver/MinesweeperSolver.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minesweeper.Solver
+{
+    public class MinesweeperSolver
+    {
+        private int[,] inputBoard;
+        private int width;
+        private int height;
+
+        public MinesweeperSolver(int width, int height, int[,] inputBoard)
+        {
+            this.width = width;
+            this.height = height;
+            this.inputBoard = inputBoard;
+        }
+
+        public int[,] SolveGame()
+        {
+            int [,] outputBoard = new int[height, width];
+            for (int row= 0;row < height;row++)
+            {
+                for(int col=0;col<width;col++)
+                {
+                    outputBoard[row, col] = GetLocationValue(row, col);
+                }
+            }
+            return outputBoard;
+        }
+
+        public int GetLocationValue(int rowIndex, int colIndex)
+        {
+            try
+            {
+                int locationValue = 0;
+                //To make the test for the corner succeed, we need to return the value -1, if the input value is -1.
+                if (inputBoard[rowIndex, colIndex] == -1)
+                    return -1;
+
+                int startRow = rowIndex - 1;
+                int endRow = rowIndex + 1;
+
+                int startCol = colIndex - 1;
+                int endCol = colIndex + 1;
+
+                if (startRow < 0)
+                    startRow = 0;
+
+                if (endRow >= height)
+                    endRow = height - 1;
+
+                if (startCol < 0)
+                    startCol = 0;
+
+                if (endCol >= width)
+                    endCol = width - 1;
+
+                for (int row = startRow; row <= endRow; row++)
+                {
+                    for (int col = startCol; col <= endCol; col++)
+                    {
+                        if (row == rowIndex && col == colIndex)
+                            continue;
+                        if (inputBoard[row, col] == -1)
+                            locationValue += 1;
+                    }
+                }
+
+                return locationValue;
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
